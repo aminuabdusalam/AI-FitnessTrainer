71c5d20b65861fb4c7b657fd70d41ac1e12013ea
Aminu Abdusalam <aminuabdusalam2019@gmail.com>
2022-10-11 21:03:50 -0500

    Added footprint for Pushups and Squats

---
+++ Exercises/Pushups.py
@@ -0,0 +1,11 @@
+
+class Pushups():
+    
+    def __init__():
+        pass 
+
+    def get_legs_angle():
+        pass 
+
+    def get_hands_angle():
+        pass 
---
+++ Exercises/Squats.py
@@ -0,0 +1,12 @@
+
+class Squats():
+    
+
+    def __init__():
+        pass 
+
+    def get_legs_angle():
+        pass 
+
+    def get_hands_angle():
+        pass 
